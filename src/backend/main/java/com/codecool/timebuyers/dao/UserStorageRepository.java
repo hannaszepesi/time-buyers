6ff7c47bc0f569dd2641d3860e1c46a0c395f219
kovesdieszter <kovesdieszter@github.com>
2022-08-17 15:34:46 +0200

    feat: implement security system

--- src/backend/main/java/com/codecool/timebuyers/dao/UserStorageRepository.java
+++ src/backend/main/java/com/codecool/timebuyers/dao/UserStorageRepository.java
@@ -8,11 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserStorageRepository extends JpaRepository<UserProfile, UUID> {
     UserProfile findByEmail(String email);
-    UserProfile findByUserName(String username);
+    Optional<UserProfile> findByUserName(String username);
     List<UserProfile> findAllByUserStatus(UserStatus userStatus);
 
     @Query(value="SELECT * FROM user_profile\n" +
