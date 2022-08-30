97d5a158e38fdf6f7562efbf5c6b2bf2007ab40a
kovesdieszter <kovesdieszter@github.com>
2022-08-30 15:44:40 +0200

    fix: implement logout

---
+++ src/frontend/timebuyers_app/src/components/Logout.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Logout(){
+    localStorage.clear();
+    localStorage
+}
+
+export default Logout;
--- src/frontend/timebuyers_app/src/components/Navbar.js
+++ src/frontend/timebuyers_app/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import '../static/CSS/Navbar.css';
 import React from "react";
 import {Link} from "react-router-dom";
+import LoggedInOrNot from "./LoggedInOrNot";
 
 function Navbar(){
     return (
@@ -33,17 +34,7 @@ function Navbar(){
                         <Link to="contact-us" className="nav-link">Contact us </Link>
                     </li>
                 </ul>
-                <ul className="navbar-nav mr-right">
-                    <li className="nav-item">
-                        <Link to={"/login"} className="nav-link"> Login </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/users/new-user"} className="nav-link">Registration</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/users/edit-profile/josh@gmail.com"} className="nav-link">Edit Profile</Link>
-                    </li>
-                </ul>
+                <LoggedInOrNot localStorageVariable={localStorage.getItem("username")} />
             </div>
         </nav>
     )
