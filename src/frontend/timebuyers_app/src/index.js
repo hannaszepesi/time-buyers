e2ba034070bd03b7c62a78357c8fcabbe14a9d0d
kovesdieszter <kovesdieszter@github.com>
2022-09-01 11:17:35 +0200

    feat: modify index structure to use usestate

--- src/frontend/timebuyers_app/src/index.js
+++ src/frontend/timebuyers_app/src/index.js
@@ -12,28 +12,37 @@ import ContactUs from "./components/ContactUs";
 import EditProfile from "./components/EditProfile";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
+import {useState} from "react";
+
+
+const TopLevel = () => {
+    const [isLoggedIn, setLoggedIn] = useState(localStorage.getItem("username") !== null);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App isLoggedIn={isLoggedIn}/>}>
+                    <Route path="tasks/:filter" element={<AllUsers/>}/>
+                    <Route path="tasks/:filter/:statusFilter" element={<AllUsers/>}/>
+                    <Route path="users/:filter" element={<AllUsers/>}/>
+                    <Route path="users/new-user" element={<RegisterForm/>}/>
+                    <Route path="about-us" element={<AboutUs/>}/>
+                    <Route path="contact-us" element={<ContactUs/>}/>
+                    <Route path="" element={<Inside/>}/>
+                    <Route path="users/edit-profile/:filter" element={<EditProfile/>}/>
+                    <Route path="/login" element={<Login setLoggedIn={setLoggedIn}/>}/>
+                    <Route path="/logout" element={<Logout setLoggedIn={setLoggedIn}/>}/>
+
+                </Route>
+
+            </Routes>
+
+        </BrowserRouter>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route path="tasks/:filter" element={<AllUsers />} />
-                <Route path="tasks/:filter/:statusFilter" element={<AllUsers />} />
-                <Route path="users/:filter" element={<AllUsers />} />
-                <Route path="users/new-user" element={<RegisterForm />} />
-                <Route path="about-us" element={<AboutUs />} />
-                <Route path="contact-us" element={<ContactUs />} />
-                <Route path="" element={<Inside />}/>
-                <Route path="users/edit-profile/:filter" element={<EditProfile />}/>
-                <Route path="/login" element={<Login />} />
-                <Route path="/logout" element={<Logout />} />
-
-            </Route>
-
-        </Routes>
-
-    </BrowserRouter>
+    <TopLevel />
 );
 
 // If you want to start measuring performance in your app, pass a function
